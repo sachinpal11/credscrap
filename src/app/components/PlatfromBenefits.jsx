fad024e1e048385a4465ddd8bec7395c2054eb64
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-03-22 10:18:35 +0530

    commit

--- src/app/components/PlatfromBenefits.jsx
+++ src/app/components/PlatfromBenefits.jsx
@@ -33,7 +33,7 @@ function PlatfromBenefits() {
     <div ref={divRef} className='w-[95%] mt-20 border-t-1 border-green-500 relative uppercase text-9xl h-[90vh] flex flex-col items-center justify-center'>
       <div className='text-lg top-10 left-10 text-green-500  absolute'>Platform features</div>
       <div>
-        <h1>Wastes to</h1>
+        <h1>Waste to</h1>
         <h1 className='flex bg-gradient-to-r from-green-500 via-green-300 to-green-500 bg-clip-text text-transparent items-center gap-4'><div className='w-40 h-[10px] bg-green-500'></div>credits</h1>
       </div>
 
